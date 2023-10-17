fd6e1f4e6365fa2faa00161d2d2e387d4f9339f6
Juliano Krindges <juliano9krindges@hotmail.com>
2023-10-17 18:52:12 -0400

    feat: testando novas cores

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 
 export const Header = () => {
   return (
-    <header className="bg-[#76b8e8] h-[60px]">
-      <div className="container ma">
-        <a href="/" className="font-700 decoration-none text-white">
+    <header className="bg-[#2E1B86] h-[60px]">
+      <div className="container ma flex items-center h-full">
+        <a href="/" className="font-700 decoration-none text-[#FFA84C]">
           Home
         </a>
       </div>
