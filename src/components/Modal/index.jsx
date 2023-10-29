8e0147c4c7bf5775cec9c2632bbcb3f66f7c460a
Juliano Krindges <juliano9krindges@hotmail.com>
2023-10-28 21:25:13 -0400

    feat: add bet

---
+++ src/components/Modal/index.jsx
@@ -0,0 +1,42 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+const BACKGROUND_STYLE = {
+  position: 'fixed',
+  top: '0',
+  bottom: '0',
+  left: '0',
+  right: '0',
+  backgroundColor: 'rgb(0,0,0, 0.7)',
+  zIndex: '1000',
+};
+
+const MODAL_STYLE = {
+  position: 'fixed',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%,-50%)',
+  padding: '10px',
+  backgroundColor: '#fff',
+  borderRadius: '10px',
+  color: 'black',
+};
+
+// eslint-disable-next-line react/prop-types
+export default function Modal({ isOpen, setModalOpen, children }) {
+  if (isOpen) {
+    return (
+      <div style={BACKGROUND_STYLE}>
+        <div style={MODAL_STYLE}>
+          <div style={{ cursor: 'pointer' }} onClick={setModalOpen}>
+            x
+          </div>
+          <div>{children}</div>
+          <button onClick={setModalOpen}>Fechar</button>
+        </div>
+      </div>
+    );
+  }
+
+  return null;
+}
--- src/modules/Dashboard/index.jsx
+++ src/modules/Dashboard/index.jsx
@@ -1,15 +1,63 @@
+import axios from 'axios';
 // eslint-disable-next-line no-unused-vars
-import React, { useContext, useEffect } from 'react';
+import React, { useCallback, useContext, useEffect, useState } from 'react';
+import Modal from 'react-modal';
 
 import Grupos from '../../assets/groups.json';
 
 import WalletContext from '../../context/wallet';
 import UserContext from '../../context/user';
 
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    width: '30rem',
+    height: '20rem',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
+
+Modal.setAppElement('#root');
+
 export const Dashboard = () => {
+  const [openModal, setOpenModal] = useState(false);
+  const [betNumber, setBetNumber] = useState(0);
+  const [betPoints, setBetPoints] = useState();
+
   const { userId } = useContext(UserContext);
 
   const { wallet, handleGetWalletValues } = useContext(WalletContext);
+
+  const handleBet = useCallback(
+    async (betNumber, bettingPoints) => {
+      // console.log(betNumber, bettingPoints);
+
+      await axios
+        .post('http://localhost:3030/api/v1/bets', {
+          userId,
+          betNumber,
+          bettingPoints: Number(bettingPoints),
+        })
+        .then((res) => {
+          console.log(res.data);
+        });
+    },
+    [userId]
+  );
+
+  const handleOpenModal = useCallback((number) => {
+    setOpenModal(true);
+    setBetNumber(number);
+    console.log(number);
+  }, []);
+
+  function closeModal() {
+    setOpenModal(false);
+  }
 
   useEffect(() => {
     const fetchData = async () => {
@@ -28,11 +76,46 @@ export const Dashboard = () => {
           <span className="text-green text-xl font-700">{wallet?.saldo}</span>
         </div>
       </header>
-      <div className="grid grid-cols-4 gap-4 mt-4">
+      <Modal
+        isOpen={openModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+      >
+        <h2>Confirmar Aposta</h2>
+        <div className="flex flex-col gap-2">
+          <span>Grupo: {betNumber}</span>
+          <label htmlFor="points" className="flex flex-col w-[20%]">
+            Pontos:{' '}
+            <input
+              type="text"
+              id="points"
+              value={betPoints}
+              onChange={(event) => setBetPoints(event.target.value)}
+              className="outline-amber bg-[#E8E8E8] px-2 py-2 rounded"
+            />
+          </label>
+        </div>
+        <footer className="fixed bottom-[10%]  flex gap-4">
+          <button
+            onClick={() => handleBet(betNumber, betPoints)}
+            className="cursor-pointer py-3 px-5 rounded font-700 bg-[#FFA84C] text-white"
+          >
+            Confirmar
+          </button>
+          <button
+            onClick={() => setOpenModal(false)}
+            className="cursor-pointer py-3 px-5 rounded font-700 bg-red text-white"
+          >
+            Cancelar
+          </button>
+        </footer>
+      </Modal>
+      <div className="grid grid-cols-4 gap-4 my-4">
         {Grupos.grupos.map((grupo) => (
           <div
+            onClick={() => handleOpenModal(grupo.grupo)}
             key={grupo.numero}
-            className="bg-[#c4c4c4] w-[100%] h-[10rem] rounded cursor-pointer"
+            className="bg-[#E0E0E0] w-[100%] h-[10rem] rounded cursor-pointer"
           >
             <div className="text-white flex flex-col justify-between h-full">
               <span className="font-700 text-4xl p2 text-black">
